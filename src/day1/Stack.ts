0281e38a588a3011ef066f475865de84021ed848
rohit singh negi <rocknegi53@gmail.com>
2024-02-17 01:14:58 +0100

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+    value: T;
+    prev: Node<T>;
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const newNode = { value: item } as Node<T>;
+        if (!this.head)
+            this.head = newNode;
+        else {
+            newNode.prev = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head)
+            return undefined;
 
-}
+        const head = this.head;
+        if (this.length > 0) {
+            this.head = this.head?.prev;
+            this.length--;
+        }
+        else {
+            this.head = undefined;
+        }
+        return head.value;
+    }
+
     peek(): T | undefined {
+        return this.head?.value;
 
-}
+    }
 }
